16574c0283b2c424b7114d480155b04d138b43c2
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-08 17:24:38 +0500

    Lesson_two_Flutter

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+//
+//
+//
+//
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Container(
+//       color: Colors.black,
+//       child: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 Container(
+//                   height: 200,
+//                   width: 190,
+//                   child: Center(
+//                       child: Text(
+//                     "Hello",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   )),
+//                   decoration: const BoxDecoration(
+//                       color: Colors.red,
+//                       borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         topLeft: Radius.circular(16),
+//                         bottomLeft: Radius.circular(16),
+//                         bottomRight: Radius.circular(16),
+//                       )),
+//                 ),
+//                 Container(
+//                   height: 200,
+//                   width: 190,
+//                   child: Center(
+//                       child: Text(
+//                         "Hello",
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             decoration: TextDecoration.none, color: Colors.white),
+//                       )),
+//                   decoration: const BoxDecoration(
+//                       color: Colors.red,
+//                       borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         topLeft: Radius.circular(16),
+//                         bottomLeft: Radius.circular(16),
+//                         bottomRight: Radius.circular(16),
+//                       )),
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Container(
+//                   height: 15,
+//                   width: 395,
+//                   color: Colors.black,
+//                 )
+//               ],
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Container(
+//                   height: 140,
+//                   width: 395,
+//                   child: Center(
+//                       child: Text(
+//                         "Hello",
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             decoration: TextDecoration.none, color: Colors.white),
+//                       )),
+//                   decoration: const BoxDecoration(
+//                       color: Colors.blue,
+//                       borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         bottomRight: Radius.circular(16),
+//                         topLeft: Radius.circular(16),
+//                         bottomLeft: Radius.circular(16),
+//                       )),
+//                 )
+//               ],
+//             ),
+//             Row(
+//               children: [
+//                 Container(
+//                   height: 15,
+//                   width: 395,
+//                   color: Colors.black,
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 Container(
+//                   height: 300,
+//                   width: 190,
+//                   child: Center(
+//                       child: Text(
+//                         "Hello",
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             decoration: TextDecoration.none, color: Colors.white),
+//                       )),
+//                   decoration: const BoxDecoration(
+//                       color: Colors.cyan,
+//                       borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         bottomRight: Radius.circular(16),
+//                         topLeft: Radius.circular(16),
+//                         bottomLeft: Radius.circular(16),
+//                       )),
+//                 ),
+//                 Container(
+//                   height: 300,
+//                   width: 190,
+//                   child: Center(
+//                       child: Text(
+//                         "Hello",
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             decoration: TextDecoration.none, color: Colors.white),
+//                       )),
+//                   decoration: const BoxDecoration(
+//                       color: Colors.cyan,
+//                       borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         bottomRight: Radius.circular(16),
+//                         topLeft: Radius.circular(16),
+//                         bottomLeft: Radius.circular(16),
+//                       )),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     ));
+//   }
+// }
+
+
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+          color: Colors.black,
+          
+        ));
+  }
+}

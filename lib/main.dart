d2727ad493ec51b3951eacb3af14fa7308711af1
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-08 19:49:06 +0500

    Yana Yangi exercise qushildi

--- lib/main.dart
+++ lib/main.dart
@@ -419,6 +419,117 @@
 //   }
 // }
 
+
+
+
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.white,
+//         child: Stack(children:[
+//           Positioned(
+//             top: 50,
+//             bottom: 600,
+//             right: 130,
+//             left: 30,
+//             child: Container(
+//               height: 50,
+//               width: 450,
+//               decoration: const BoxDecoration(
+//                   color: Colors.green,
+//                   borderRadius: BorderRadius.only(
+//                     bottomRight: Radius.circular(20),
+//                     bottomLeft: Radius.circular(20),
+//                     topRight: Radius.circular(20),
+//                     topLeft: Radius.circular(20),
+//                   )),
+//               child: Text(
+//                 "Green",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                     decoration: TextDecoration.none,
+//                     color: Colors.white,
+//                     fontSize: 20),
+//               ),
+//             ),
+//           ),
+//
+//
+//           Positioned(
+//             top: 100,
+//             bottom: 530,
+//             right: 80,
+//             left: 80,
+//             child: Container(
+//               height: 50,
+//               width: 450,
+//               decoration: const BoxDecoration(
+//                   color: Colors.red,
+//                   borderRadius: BorderRadius.only(
+//                     bottomRight: Radius.circular(20),
+//                     bottomLeft: Radius.circular(20),
+//                     topRight: Radius.circular(20),
+//                     topLeft: Radius.circular(20),
+//                   )),
+//               child: Text(
+//                 "red",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                     decoration: TextDecoration.none,
+//                     color: Colors.white,
+//                     fontSize: 20),
+//               ),
+//             ),
+//           ),
+//
+//
+//           Positioned(
+//             top: 160,
+//             bottom: 470,
+//             right: 20,
+//             left: 130,
+//             child: Container(
+//               height: 50,
+//               width: 450,
+//               decoration: const BoxDecoration(
+//                   color: Colors.purple,
+//                   borderRadius: BorderRadius.only(
+//                     bottomRight: Radius.circular(20),
+//                     bottomLeft: Radius.circular(20),
+//                     topRight: Radius.circular(20),
+//                     topLeft: Radius.circular(20),
+//                   )),
+//               child: Text(
+//                 "Purple",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                     decoration: TextDecoration.none,
+//                     color: Colors.white,
+//                     fontSize: 20),
+//               ),
+//             ),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -434,66 +545,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Container(
         color: Colors.white,
-        child: Stack(children: [
+        child: Stack(children:[
           Positioned(
-            top: 50,
-            bottom: 600,
-            right: 130,
+            top: 250,
+            bottom: 300,
+            right: 90,
+            left: 90,
+            child: Container(
+              height: 50,
+              width: 450,
+              decoration: const BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  )),
+              child: Text(
+                "Green",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.white,
+                    fontSize: 20),
+              ),
+            ),
+          ),
+
+
+
+          Positioned(
+            top: 150,
+            bottom: 200,
+            right: 150,
+            left: 150,
+            child: Container(
+              height: 50,
+              width: 450,
+              decoration: const BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  )),
+
+            ),
+          ),
+
+
+          Positioned(
+            top: 350,
+            bottom: 450,
+            right: 30,
             left: 30,
             child: Container(
               height: 50,
               width: 450,
-              color: Colors.green,
-              child: Text(
-                "Green",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    color: Colors.white,
-                    fontSize: 20),
-              ),
-            ),
-          ),
-
-
-
-          Positioned(
-            top: 100,
-            bottom: 530,
-            right: 80,
-            left: 80,
-            child: Container(
-              height: 50,
-              width: 450,
-              color: Colors.red,
-              child: Text(
-                "red",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    color: Colors.white,
-                    fontSize: 20),
-              ),
-            ),
-          ),
-
-          Positioned(
-            top: 160,
-            bottom: 470,
-            right: 20,
-            left: 120,
-            child: Container(
-              height: 50,
-              width: 450,
-              color: Colors.purple,
-              child: Text(
-                "Purple",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    color: Colors.white,
-                    fontSize: 20),
-              ),
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  )),
             ),
           ),
 
